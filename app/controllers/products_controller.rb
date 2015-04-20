795d1612eace7135c42112e4824e0c2a9e66ac59
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-04-20 16:07:49 -0400

    Displaying validation error for product form, where 'presence: true' for all fields

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-
+    # binding.pry
     if @product.save
       redirect_to products_url
     else
