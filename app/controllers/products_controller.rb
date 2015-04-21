fa8d733651bd21e4a5ae2c8deecb1db85b120187
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-04-21 14:28:23 -0400

    displaying prics in dollars

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,11 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  # def formatted_price
+  #   price_in_dollars = price_in_cents.to_f / 100
+  #   sprintf("%.2f", price_in_dollars)
+  # end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price_in_cents)
