e27306435c0671bc96c3f155c886c09ac15ac6dd
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-04-21 15:24:24 -0400

    Creating user model, and controller and view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(email: "josh@example.com", password: "asdf", password_confirmation: "asdf")
