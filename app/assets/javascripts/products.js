999122da51b5f49b355da7ed0fd8e153e378fdeb
NIKHAT BHAYANI <NIKHATALY@GMAIL.COM>
2015-05-10 11:45:17 -0400

    Partial is rendered with hq .get method

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,36 +3,10 @@ $(document).on('ready page:load', function(){
     event.preventDefault();
     var searchValue = $('#search').val();
 
-    $.ajax ({
-      url: '/products?search=' + searchValue,
-      type: 'GET',
-      dataType: 'html'
-    }).done(function(data){
+    $.get('/products?search=' + searchValue)
+    .done(function(data){
       $('products').html(data);
     });
   });
 });
 
-// $(document).on('ready page:load', function() {
-//   $('#search-form').submit(function(event) {
-//     event.preventDefault();
-//     var searchValue = $('#search').val();
-
-//     $.ajax({
-//       url: '/products?search=' + searchValue,
-//       type: 'GET',
-//       dataType: 'html'
-//     }).done(function(data){
-//       $('#products').html(data);
-//     });
-//   });
-// });
-
-//   ...
-//     // inside the submit event callback, replace the $.ajax portion with the following
-//     $.get('/products?search=' + searchValue)
-//       .done(function(data){
-//         console.log(data);
-//         $('#products').html(data);
-//       });
-//   ...
